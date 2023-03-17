ca9a3a14532007733b52e6b1f06fe1632737b0b9
Roland Akakpo <108545036+rakakp007@users.noreply.github.com>
2023-03-17 13:24:01 -0500

    Add files via upload

---
+++ Folder_compressor.sh
@@ -0,0 +1,57 @@
+#!/bin/bash 
+#SBATCH --time=96:00:00
+#SBATCH --ntasks=8
+#SBATCH --mem=10g
+#SBATCH --tmp=10g
+#SBATCH --mail-type=ALL  
+#SBATCH --mail-user=rakakpo@umn.edu 
+
+
+#   Requires the one arguments: the path to the folder to be compressed
+
+if [ "$1" == "-h" ]; then
+
+  echo " "
+  echo "###############################"
+  echo "#### The command line help ####"
+  echo "###############################"
+  
+  echo " "
+  
+  echo "description: compress a whole folder"
+  
+  echo " "
+  
+  echo "arg1 = Full path to folder to be compressed"
+  
+  echo " "
+  
+  echo "compressed folder name =   foldername_date"
+  
+  echo " "
+  
+  echo "#### rakakpo@umn.edu ####"
+   
+  echo " "
+  
+  exit 1
+  
+fi
+
+if [[ -z "$1" ]]; then 
+  echo "Warning Argument is missing" 
+  exit 0 
+fi
+
+if 
+
+[[ -f "$1" || -d "$1" ]]; then  
+  
+  echo " "
+  
+  echo "Processing:"
+  
+  echo " "
+  
+  tar -zcvf $1"_"$(date '+%d-%m-%y').tar.gz $1
+fi
