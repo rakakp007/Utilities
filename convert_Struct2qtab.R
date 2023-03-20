fbab726c72a982c9d6cbd0b937ed726377f1bb28
Roland Akakpo <108545036+rakakp007@users.noreply.github.com>
2023-03-20 10:08:29 -0500

    Add files via upload

---
+++ convert_Struct2qtab.R
@@ -0,0 +1,9 @@
+# Used to run the convertion of structure output in a usable table
+# rakakpo
+# January 2023
+#St. Paul - UMN 
+
+
+source("~/MorrellLab Dropbox/Roland Akakpo/IITA_CORE_DATA/cowpea_gwas-main/STRUCTURES/STRUCTURE_PITCHARD/Script/struct2qtab.R")
+struct2qtab("../Data/cowpea_snp_for_structure_pruned_data.recode.strct_in","./results_WA/cowpea_struct_k5_run1_f")
+
