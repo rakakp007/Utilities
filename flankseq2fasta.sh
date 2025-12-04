ddb1cf20a0265b7ccaf9d9264fb40b795778f5f8
Roland Akakpo <108545036+rakakp007@users.noreply.github.com>
2025-12-04 16:33:32 +0100

    Add files via upload

---
+++ flankseq2fasta.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#Write by Rakakpo/Ifremer 2025-11-20 - La Tremblade - France
+#This script allow converting flaking sequence from axiom annotation file
+#into a fasta file for mapping to get SNP acconding to a reference genome
+
+
+INPUT="/home1/datahome/rakakpo/Mapping_Ref/Axiom_Oyster02.Annotation.r1.CG.csv"
+OUTPUT="/home1/datahome/rakakpo/Mapping_Ref/Axiom_Oyster02.Annotation.fa"
+
+rm -f "$OUTPUT"
+
+# Skip first 19 lines (metadata + header)
+tail -n +20 "$INPUT" | while IFS=';' read -r -a cols
+do
+    ProbeSetID="${cols[0]}"
+    Flank="${cols[5]}"      # column 6 in the CSV
+
+    # Replace [A/G] with A (first allele)
+    SEQ=$(echo "$Flank" | sed -E 's/\[([A-Z])\/[A-Z]\]/\1/')
+
+    echo ">${ProbeSetID// /_}" >> "$OUTPUT"
+    echo "$SEQ" >> "$OUTPUT"
+done
+
+echo "FASTA file created: $OUTPUT"
